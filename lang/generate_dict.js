cd7a6bd472b857147a630c0451f72a019f4940c2
schwert398 <ritterderschwerter178@gmail.com>
2021-12-10 14:02:25 +0900

    fix link

--- lang/generate_dict.js
+++ lang/generate_dict.js
@@ -1,40 +1,33 @@
 "use strict";
-var get_word = function (dictionary, id) {
-    var word = dictionary.words.filter(function (a) { return a.entry.id === id; })[0];
-    var html_element_id = "id" + id + "_" + word.entry.form.split(" ").join("_");
-    var word_form = "<div><div class=\"word_form\">" + word.entry.form + "</div><div class=\"tags\">" + word.tags.map(function (a) { return '<span class="bordered_info">' + a + '</span>'; }).join("") + "</div><a id=\"permalink_" + html_element_id + "\" href=\"#" + html_element_id + "\" class=\"permalink\">\u00B6</a></div>";
-    var translations = "<div class=\"word_infos\">" + word.translations.map(function (t) { return '<p class="word_info"><span class="bordered_info">' + t.title + '</span>' + t.forms.join(", ") + '</p>'; }).join("") + "</div>";
-    var contents = word.contents.map(function (_a) {
-        var title = _a.title, text = _a.text;
-        return '<div class="word_infos"><p class="word_info"><span class="nonbordered_info">' + title + '</span>' + text + '</p></div>';
-    }).join("");
-    var relations = "<div class=\"word_infos\">" + word.relations.map(function (_a) {
-        var title = _a.title, entry = _a.entry;
-        return '<p class="word_info">→<span class="bordered_info">' + title + '</span><a href="#id' + entry.id + '_' + entry.form + '">' + entry.form + '</a></p>';
-    }).join("") + "</div>";
-    return "<div class=\"word\" id=\"" + html_element_id + "\" onmouseover=\"document.getElementById('permalink_" + html_element_id + "').style.visibility = 'visible'\" onmouseout=\"document.getElementById('permalink_" + html_element_id + "').style.visibility = 'hidden'\">" + (word_form + translations + contents + relations) + "</div>";
+const get_word = (dictionary, id) => {
+    const [word] = dictionary.words.filter(a => a.entry.id === id);
+    const html_element_id = `id${id}_${word.entry.form.split(" ").join("_")}`;
+    const word_form = `<div><div class="word_form">${word.entry.form}</div><div class="tags">${word.tags.map(a => '<span class="bordered_info">' + a + '</span>').join("")}</div><a id="permalink_${html_element_id}" href="#${html_element_id}" class="permalink">¶</a></div>`;
+    const translations = `<div class="word_infos">${word.translations.map(t => '<p class="word_info"><span class="bordered_info">' + t.title + '</span>' + t.forms.join(", ") + '</p>').join("")}</div>`;
+    const contents = word.contents.map(({ title, text }) => '<div class="word_infos"><p class="word_info"><span class="nonbordered_info">' + title + '</span>' + text + '</p></div>').join("");
+    const relations = `<div class="word_infos">${word.relations.map(({ title, entry }) => '<p class="word_info">→<span class="bordered_info">' + title + '</span><a href="#id' + entry.id + '_' + entry.form + '">' + entry.form + '</a></p>').join("")}</div>`;
+    return `<div class="word" id="${html_element_id}" onmouseover="document.getElementById('permalink_${html_element_id}').style.visibility = 'visible'" onmouseout="document.getElementById('permalink_${html_element_id}').style.visibility = 'hidden'">${word_form + translations + contents + relations}</div>`;
 };
-var render = function (dictionary) {
+const render = (dictionary) => {
     var _a;
-    var _b;
-    var urlParams = new URLSearchParams(window.location.search);
-    var sortBy = (_b = urlParams.get('sortBy')) === null || _b === void 0 ? void 0 : _b.toLowerCase();
-    var ids = dictionary.words.map(function (a) { return a.entry.id; });
+    const urlParams = new URLSearchParams(window.location.search);
+    const sortBy = (_a = urlParams.get('sortBy')) === null || _a === void 0 ? void 0 : _a.toLowerCase();
+    let ids = dictionary.words.map(a => a.entry.id);
     if (sortBy === "random") {
-        for (var i = ids.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            _a = [ids[j], ids[i]], ids[i] = _a[0], ids[j] = _a[1];
+        for (let i = ids.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [ids[i], ids[j]] = [ids[j], ids[i]];
         }
     }
     else if (sortBy === "id") {
-        ids = ids.sort(function (a, b) { return a - b; });
+        ids = ids.sort((a, b) => a - b);
     }
     else {
-        ids = ids.sort(function (a, b) {
-            var w_a = dictionary.words.filter(function (k) { return k.entry.id === a; })[0];
-            var w_b = dictionary.words.filter(function (k) { return k.entry.id === b; })[0];
+        ids = ids.sort((a, b) => {
+            const [w_a] = dictionary.words.filter(k => k.entry.id === a);
+            const [w_b] = dictionary.words.filter(k => k.entry.id === b);
             return w_a.entry.form === w_b.entry.form ? 0 : w_a.entry.form > w_b.entry.form ? 1 : -1;
         });
     }
-    document.getElementById("outer").innerHTML = ids.map(function (id) { return get_word(dictionary, id); }).join("");
+    document.getElementById("outer").innerHTML = ids.map(id => get_word(dictionary, id)).join("");
 };
--- lang/generate_wordlist.js
+++ lang/generate_wordlist.js
@@ -1,25 +1,22 @@
 "use strict";
-var get_word_for_list = function (dictionary, id, word_class) {
-    var word = dictionary.words.filter(function (a) { return a.entry.id === id; })[0];
-    var html_element_id = "id" + id + "_" + word.entry.form.split(" ").join("_");
-    var word_form = "<td>" + id + "</td><td><a href=\"./dict.html#" + html_element_id + "\">" + word.entry.form + "</a></td>";
-    var translations = word_class == "全単語" ? "<td class=\"meaning\">" + word.translations.map(function (t) { return t.forms.join(", "); }).join("；") + "</td>" : "<td class=\"meaning\">" + word.translations.filter(function (t) { return t.title == word_class; }).map(function (t) { return t.forms.join(", "); }).join("、") + "</td>";
-    return translations == '<td class="meaning"></td>' ? "" : "<tr>" + word_form + translations + "</tr>";
+const get_word_for_list = (dictionary, id, word_class) => {
+    const [word] = dictionary.words.filter(a => a.entry.id === id);
+    const html_element_id = `id${id}_${word.entry.form.split(" ").join("_")}`;
+    const word_form = `<td>${id}</td><td><a href="./dict.html#${html_element_id}">${word.entry.form}</a></td>`;
+    const translations = word_class == "全単語" ? `<td class="meaning">${word.translations.map(t => t.forms.join(", ")).join("；")}</td>` : `<td class="meaning">${word.translations.filter(t => t.title == word_class).map(t => t.forms.join(", ")).join("、")}</td>`;
+    return translations == '<td class="meaning"></td>' ? "" : `<tr>${word_form}${translations}</tr>`;
 };
-var word_count = function (word_class_id) {
+const word_count = (word_class_id) => {
     return document.getElementById(word_class_id).children[1].childElementCount;
 };
-var render_for_list = function (dictionary) {
-    var word_classes = ["全単語", "名詞", "代名詞", "動詞", "形容詞", "助辞"];
-    var word_classes_id = { "全単語": "all", "名詞": "noun", "代名詞": "pronoun", "動詞": "verb", "形容詞": "modifier", "助辞": "particle" };
-    var ids = dictionary.words.map(function (a) { return a.entry.id; });
-    var _loop_1 = function (i) {
-        var word_class = word_classes[i];
-        var word_class_id = word_classes_id[word_class];
-        document.getElementById(word_class_id).innerHTML += ids.map(function (id) { return get_word_for_list(dictionary, id, word_class); }).join("");
-        document.getElementById(word_class_id + "_summary").innerHTML += "\u3000" + word_count(word_class_id) + "\u8A9E";
-    };
-    for (var i = 0; i < word_classes.length; i++) {
-        _loop_1(i);
+const render_for_list = (dictionary) => {
+    const word_classes = ["全単語", "名詞", "代名詞", "動詞", "形容詞", "助辞"];
+    const word_classes_id = { "全単語": "all", "名詞": "noun", "代名詞": "pronoun", "動詞": "verb", "形容詞": "modifier", "助辞": "particle" };
+    let ids = dictionary.words.map(a => a.entry.id);
+    for (let i = 0; i < word_classes.length; i++) {
+        const word_class = word_classes[i];
+        const word_class_id = word_classes_id[word_class];
+        document.getElementById(word_class_id).innerHTML += ids.map(id => get_word_for_list(dictionary, id, word_class)).join("");
+        document.getElementById(`${word_class_id}_summary`).innerHTML += `　${word_count(word_class_id)}語`;
     }
 };
